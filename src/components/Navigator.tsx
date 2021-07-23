19b1fb2f8d63b704dd8126cf363b23ddd1af33bd
HaJunRyu <hjr4557@naver.com>
2021-07-23 22:31:37 +0900

    Modify: starter kit의 template을 컴포넌트 파일로 생성
    Changes to be committed:
    new file:   src/components/Footer.tsx
    new file:   src/components/Navigator.tsx
    new file:   src/pages/Article.tsx
    new file:   src/pages/Auth.tsx
    new file:   src/pages/EditArticle.tsx
    new file:   src/pages/Home.tsx
    new file:   src/pages/Profile.tsx
    new file:   src/pages/Settings.tsx

---
+++ src/components/Navigator.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function Navigator() {
+  return (
+    <nav className="navbar navbar-light">
+      <div className="container">
+        <a className="navbar-brand" href="/#/">
+          conduit
+        </a>
+        <ul className="nav navbar-nav pull-xs-right">
+          <li className="nav-item">
+            <a className="nav-link active" href="/#/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/article">
+              <i className="ion-compose"></i>&nbsp;New Post
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/#/settings">
+              <i className="ion-gear-a"></i>&nbsp;Settings
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/#/register">
+              Sign up
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
